19987e926d22f8a7bf7053227f123e827d0838ac
grizz12 <raka_088@smkassalaambandung.sch.id>
2023-03-17 22:15:48 +0700

    login session auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
-export class Login extends Component {
-  static propTypes = {};
+const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
 
-  render() {
-    return (
-      <div className="formContainer">
-        <div className="formWrapper">
-          <span className="logo">Super Chat</span>
-          <span className="title">Login</span>
-          <form>
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="password" />
-            <button>Sign in</button>
-            <p>Tidak punya akun? Register</p>
-          </form>
-        </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Super Chat</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Masuk</button>
+          {err && <span>Ada yang salah</span>}
+        </form>
+        <p>
+          Anda tidak memiliki akun ? <Link to="/register">Register</Link>
+        </p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Login;
