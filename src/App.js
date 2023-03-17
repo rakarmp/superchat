74d134546bc1166b52ff2276b28fc3908d471442
grizz12 <raka_088@smkassalaambandung.sch.id>
2023-03-17 11:27:39 +0700

    Recode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import './style.scss';
 
 function App() {
   return (
-    <Register/>
+    <Home />
   );
 }
 
