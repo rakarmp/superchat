a783b3196c8ff39d41bf26a07abb093356840476
grizz12 <raka_088@smkassalaambandung.sch.id>
2023-03-19 04:46:25 +0700

    fix bug timestamp

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -7,16 +7,32 @@ import React, {
 } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
+import dayjs from "dayjs";
 
 const Message = ({ message }) => {
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
+  const [time, setTime] = useState("");
 
   const ref = useRef();
 
   useEffect(() => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   }, [message]);
+
+  const getTime = useCallback(() => {
+    const sentAt = message.date.toDate(); // convert firestore timestamp to JavaScript Date object
+    return dayjs(sentAt).format("HH:mm");
+  }, [message]);
+
+  useEffect(() => {
+    setTime(getTime());
+    const timer = setInterval(() => {
+      setTime(getTime());
+    }, 60000); // update every 1 minute
+
+    return () => clearInterval(timer);
+  }, [getTime]);
 
   return (
     <div
@@ -32,6 +48,7 @@ const Message = ({ message }) => {
           }
           alt=""
         />
+        <span>{time}</span>
       </div>
       <div className="messageContent">
         <p>{message.text}</p>
