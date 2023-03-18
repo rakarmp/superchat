cc575db6a591a0bc79da0b7311f2f291ba901ef8
grizz12 <raka_088@smkassalaambandung.sch.id>
2023-03-18 15:12:50 +0700

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,16 @@ import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
+import Logo from "../img/logo.png";
 
 const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
 
   return (
     <div className="navbar">
-      <span className="logo">Super Chat</span>
+      <span className="logo">
+        <img src={Logo} alt="superchat" width={500} height={150} />
+      </span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
