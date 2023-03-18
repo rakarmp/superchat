14ee34a12fe6268c870797e319e30bcf6d4f552b
grizz12 <raka_088@smkassalaambandung.sch.id>
2023-03-18 17:32:27 +0700

    add icons and hide sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
 import Navbar from "./Navbar";
 import Search from "./Search";
 import Chats from "./Chats";
 
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const handleToggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   return (
-    <div className="sidebar">
-      <Navbar />
-      <Search />
-      <Chats />
+    <div className="hide-sidebar">
+      <div className="hide-icons">
+        <FaBars onClick={handleToggleSidebar}>
+          {showSidebar ? "Hide Sidebar" : "Show Sidebar"}
+        </FaBars>
+      </div>
+      {showSidebar && (
+        <div className="sidebar">
+          <Navbar />
+          <Search />
+          <Chats />
+        </div>
+      )}
     </div>
   );
 };
